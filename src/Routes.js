184cf5fd56e7e8cd40ee7dfa9a30d4d873e7c1d1
Seung Hyun Soun <jamessoun93@gmail.com>
2020-01-06 20:58:25 +0900

    불필요 파일 삭제 및 개발에 사용할 폰트파일 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 
 const Routes = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" component={Home} />
-            </Switch>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default Routes;
